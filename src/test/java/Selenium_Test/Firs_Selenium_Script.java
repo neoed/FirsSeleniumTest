b571b74c12bcf56627e091302576f57e0be80e44
neoed <neoed@tlen.pl>
2022-08-16 14:17:04 +0200

    Firs working script

--- src/test/java/Selenium_Test/Firs_Selenium_Script.java
+++ src/test/java/Selenium_Test/Firs_Selenium_Script.java
@@ -2,22 +2,32 @@ package Selenium_Test;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Open_Close_Browser {
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Firs_Selenium_Script {
     public WebDriver driver;
     @BeforeAll
     public static void setDriver() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\jerem\\IdeaProjects\\chromedriver.exe");
 
     }
-
     @Test
-    public void chromeSession() {
+    public void assertTrueTitle() {
         ChromeOptions options = new ChromeOptions();
         driver = new ChromeDriver(options);
+        driver.get("https://jbzd.com.pl//");
+
+        String title = driver.getTitle();
+        assertTrue(title.contains("obrazki"));
 
         driver.quit();
     }
